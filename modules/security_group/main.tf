0a1bc5387d6c49d3cd3ee95fbc7805d990b8dd98
bpamos <bpamos21@gmail.com>
2025-06-11 19:49:40 -0500

    application ec2 sg update

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "elasticache" {
     security_groups = [aws_security_group.riot_ec2.id]
   }
 
+  ingress {
+    description     = "Allow Redis access from Flask app EC2"
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_application.id]
+  }
   egress {
     description = "Allow all outbound"
     from_port   = 0
@@ -73,6 +80,53 @@ resource "aws_security_group" "elasticache" {
 
   tags = {
     Name    = "${var.name_prefix}-elasticache-sg"
+    Owner   = var.owner
+    Project = var.project
+  }
+}
+
+
+#### Leaderboard App
+
+resource "aws_security_group" "ec2_application" {
+  name        = "${var.name_prefix}-ec2-app-sg"
+  description = "Security group for Flask leaderboard EC2 app"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allow_ssh_from
+  }
+
+  ingress {
+    description = "Allow HTTP access to Flask app"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = var.allow_ssh_from
+  }
+
+    ingress {
+    description = "Allow HTTP access to Cutover UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = var.allow_ssh_from
+  }
+
+  egress {
+    description = "Allow all outbound (needed to reach Redis)"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.name_prefix}-ec2-app-sg"
     Owner   = var.owner
     Project = var.project
   }
--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -5,3 +5,10 @@ output "riot_ec2_sg_id" {
 output "elasticache_sg_id" {
   value = aws_security_group.elasticache.id
 }
+
+### Leaderboard App
+
+output "ec2_application_sg_id" {
+  description = "ID of the security group used for the application EC2 instance"
+  value       = aws_security_group.ec2_application.id
+}
